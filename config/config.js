c5cd579382d6f1b6f125930890a925b472667f9f
Sebastian Dang <s1dang@ucsd.edu>
2017-04-04 11:40:35 -0700

    cleaned up

--- config/config.js
+++ config/config.js
@@ -15,11 +15,11 @@
 var CONFIG = CONFIG || {};
 
 CONFIG.prompt = function(cwd, user) {
-   if (user)
-      return '<span class="user">' + user +
-          '</span>@<span class="host">github.io</span>:<span class="cwd">' +
-          cwd + '</span>$ ';
-   return 'jsterm1.0 $ ';
+    if (user)
+        return '<span class="user">' + user +
+            '</span>@<span class="host">github.io</span>:<span class="cwd">' +
+            cwd + '</span>$ ';
+    return 'jsterm1.0 $ ';
 };
 
 CONFIG.username = 'sebastian';
